2cddbf322c2a0cb23961e2a49f01b4edafaa00dd
Aman Rathod <as7677379564@gmail.com>
2021-03-26 12:01:40 +0530

    initial commit

---
+++ JavaScript30/1-Drum-KIT/script.js
@@ -0,0 +1,35 @@
+// let keys = document.querySelector('.key');
+// let sounds = document.querySelector('.sound');
+// let audios = document.querySelector('audio');
+
+// for(let i = 0; i< keys.length; ++i){
+//     keys[i].addEventListener('keydown', function (e){
+
+//         var value = this.getAttribute('data-key');
+//         for(let j = 0; j < audios.length; ++j){
+//             let aud = this.getAttribute(value);
+//             aud.play();
+//         }
+
+//     });
+
+// }
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
+
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
