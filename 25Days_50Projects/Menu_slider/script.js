198631bd88e733c52bb8c55a1d64a63aa701e651
Aman Rathod <as7677379564@gmail.com>
2021-04-08 11:22:25 +0530

    Initial Commit

---
+++ 25Days_50Projects/Menu_slider/script.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+const navbar = document.getElementById('navbar');
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+});
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+})
+
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+});
+
+
+// Hide modal on outside click
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false
+);
